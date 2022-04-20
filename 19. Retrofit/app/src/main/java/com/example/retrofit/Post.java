8486e37432d3ed6d3c010a21e27abb8675d5a34a
AnggaGemilang <anggagemilang12@gmail.com>
2022-04-20 10:12:10 +0700

    rest api done

---
+++ 19. Retrofit/app/src/main/java/com/example/retrofit/Post.java
@@ -0,0 +1,30 @@
+package com.example.retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private final int userId;
+    private Integer id;
+    private final String title;
+
+    @SerializedName("body")
+    private final String text;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getText() {
+        return text;
+    }
+}
---
+++ JobService/app/src/main/java/com/example/jobservice/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.jobservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+
+        // Set up the notification content intent to launch the app when clicked
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+    }
+}
